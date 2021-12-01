261c6a777d39199f862daf8a56ecec4e58708d26
Philipp <philipp4thiel@gmail.com>
2021-12-02 00:00:21 +0100

    Day 1

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    Y21D01();
+}
+
+fn Y21D01() {
+    let file = File::open("inputFiles/Y21D01.in").expect("file wasn't found.");
+    let reader = BufReader::new(file);
+    let numbers: Vec<i32> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<i32>().unwrap())
+        .collect();
+    // totally not copied from googling "how to read ints" :kekw:
+
+    let mut last = i32::MAX;
+    let mut res = 0;
+
+    for number in numbers.iter() {
+        if *number > last {
+            res += 1;
+        }
+        last = *number;
+    }
+    dbg!(res);
+
+    last = i32::MAX;
+    res = 0;
+
+    for i in 2..numbers.len() {
+        let cur = numbers[i] + numbers[i - 1] + numbers[i - 2];
+        if cur > last {
+            res += 1;
+        }
+        last = cur;
+    }
+    dbg!(res);
+}
