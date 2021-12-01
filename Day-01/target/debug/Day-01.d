c713e629f9fe0563c9023ac7c6b3ac62fcf0667c
Philipp <philipp4thiel@gmail.com>
2021-12-02 00:39:24 +0100

    just commit everything to have same settings :wesmart:

---
+++ Day-01/target/debug/Day-01.d
@@ -0,0 +1,1 @@
+C:\Users\phili\Documents\CodingProjekts\AoC-Rust\Day-01\target\debug\Day-01.exe: C:\Users\phili\Documents\CodingProjekts\AoC-Rust\Day-01\src\main.rs
---
+++ Day-01/target/debug/deps/Day_01.d
@@ -0,0 +1,5 @@
+C:\Users\phili\Documents\CodingProjekts\AoC-Rust\Day-01\target\debug\deps\Day_01.exe: src\main.rs
+
+C:\Users\phili\Documents\CodingProjekts\AoC-Rust\Day-01\target\debug\deps\Day_01.d: src\main.rs
+
+src\main.rs:
