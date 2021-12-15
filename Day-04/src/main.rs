c636c4e1f5d14a0a4e43bc27dc4b30f9f96a1cac
Philipp <philipp.thiel@hispeed.ch>
2021-12-16 00:53:29 +0100

    Day 4

--- Day-04/src/main.rs
+++ Day-04/src/main.rs
@@ -1,119 +1,89 @@
-use std::ffi::c_void;
 use std::fs::File;
-use std::io;
-use std::io::{BufRead, BufReader, empty};
-use std::ptr::null;
+use std::io::{BufRead, BufReader};
 
-#[derive(Clone, Copy)]
-struct Board {
-    board: [i8; 25],
-    marked: [bool; 25],
-    turn: i32,
-}
+const NUM_BOARDS: usize = 100;
+// 3 for testing 100 for actual test
 
 fn main() {
-    let file = File::open("test.in").expect("file wasn't found.");
+    let file = File::open("main.in").expect("file wasn't found.");
     let reader = BufReader::new(file);
     let mut input: Vec<String> = reader
         .lines()
         //.map(|line| line.unwrap().parse::<instruction>().unwrap())
-        .filter_map(io::Result::ok)
+        .filter_map(Result::ok)
         .filter(|s| !s.eq(""))
         .collect();
 
-    let mut i: i8 = 0;
-    let mut boards: Vec<Board> = Vec::new();
+    let numbers: Vec<u8> = input.swap_remove(0).split(",").map(|s| s.trim())
+        .filter(|s| !s.is_empty()).map(|s| s.parse::<u8>().unwrap()).collect();
 
-    let numbers: Vec<i8> = input.swap_remove(0)
-        .split(",")
-        .map(|s| s.trim())
-        .filter(|s| !s.is_empty())
-        .map(|s| s.parse::<i8>().unwrap())
-        .collect();
+    let mut boards: [[[u8; 5]; 5]; NUM_BOARDS] = [[[0; 5]; 5]; NUM_BOARDS];
+    let mut marked: [[[bool; 5]; 5]; NUM_BOARDS] = [[[false; 5]; 5]; NUM_BOARDS];
 
-    let mut all_boards: Vec<Board> = Vec::new();
-
-    let mut i: i8 = 0;
-    let mut temp_string: String = String::new();
-    while !input.is_empty() {
-        i += 1;
-        temp_string += &input.swap_remove(0);
-        temp_string += " ";
-        if i == 5 {
-            let mut temp: [i8; 25] = [0; 25];
-            let mut n: usize = 0;
-
-            for k in temp_string.trim()
-                .split(" ")
-                .map(|s| s.trim())
-                .filter(|s| !s.is_empty())
-                .map(|s| s.parse::<i8>().unwrap()) {
-                temp[n] = k;
-                n += 1;
+    for board in 0..NUM_BOARDS {
+        for i in 0..5 {
+            let cur_numbers: Vec<u8> = input.swap_remove(0).trim().split(" ")
+                .map(|s| s.trim()).filter(|s| !s.is_empty())
+                .map(|s| s.parse::<u8>().unwrap()).collect();
+            for j in 0..5 {
+                boards[NUM_BOARDS - 1 - board][4 - i][j] = cur_numbers[j];
             }
-            let mut board = Board {
-                board: temp,
-                marked: [false; 25],
-                turn: 0,
-            };
-
-            all_boards.push(board);
-
-            temp_string = String::new();
-            i = 0;
         }
     }
 
-    for n in numbers {
-        for b in &all_boards {
-            play(*b, n);
+    let mut task1 = false;
+    let mut win_counter: u8 = 0;
+    let mut done: [bool; NUM_BOARDS] = [false; NUM_BOARDS];
+    for number in numbers.clone() {
+        // play move on all boards
+        for b in 0..NUM_BOARDS {
+            for i in 0..5 {
+                for j in 0..5 {
+                    if boards[b][i][j] == number {
+                        marked[b][i][j] = true;
+                    }
+                }
+            }
         }
-    }
-    let mut low_board: Board;
-    let mut low_turn: i32 = i32::MAX;
-    let mut high_board: Board;
-    let mut high_turn: i32 = i32::MIN;
-    for b in all_boards {
-        if b.turn < low_turn {
-            low_turn = b.turn;
-            low_board = b;
-        }
-        if b.turn > high_turn {
-            high_turn = b.turn;
-            high_board = b;
-        }
-    }
 
-    dbg!(low_turn);
-    dbg!(high_turn);
+        // check for win
+        for b in 0..NUM_BOARDS {
+            if !done[b] && won(&marked[b]) {
+                done[b] = true;
+                win_counter += 1;
+                if win_counter == NUM_BOARDS as u8 {
+                    println!("Task 2: {}", get_score(&boards[b], &marked[b]) * (number as u32));
+                }
+                if !task1 {
+                    println!("Task 1: {}", get_score(&boards[b], &marked[b]) * (number as u32));
+                    task1 = true;
+                }
+            }
+        }
+    }
 }
 
-//fn get_score(marked: [bool; 25], nums: [i8; 25]) -> i32 {}
-
-fn play(mut board: Board, n: i8) -> i32 {
-    if !won(board.marked) { return board.turn; }
-    for i in 0..25 {
-        if board.board[i] == n {
-            board.marked[i] = true;
-            return board.turn + 1;
+fn get_score(board: &[[u8; 5]; 5], marked: &[[bool; 5]; 5]) -> u32 {
+    let mut unmarked: u32 = 0;
+    for i in 0..5 {
+        for j in 0..5 {
+            if !marked[i][j] {
+                unmarked += board[i][j] as u32;
+            }
         }
     }
-    return board.turn + 1;
+    return unmarked;
 }
 
-fn won(marked: [bool; 25]) -> bool {
-    let mut diag1: bool = true;
-    let mut diag2: bool = true;
+fn won(marked: &[[bool; 5]; 5]) -> bool {
     for i in 0..5 {
         let mut hor: bool = true;
         let mut ver: bool = true;
         for j in 0..5 {
-            hor = hor && marked[i * 5 + j];
-            ver = ver && marked[i + j * 5];
+            hor = hor && marked[i][j];
+            ver = ver && marked[j][i];
         }
         if hor || ver { return true; }
-        diag1 = diag1 && marked[6 * i];
-        diag2 = diag2 && marked[4 + 4 * i];
     }
-    return diag1 || diag2;
+    return false;
 }
