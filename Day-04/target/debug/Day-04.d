b05984a83b597d6e244a6458dec57e6195fd2491
Philipp4Thiel <71552729+Philipp4Thiel@users.noreply.github.com>
2021-12-16 16:00:54 +0100

    Delete Day-04/target directory

--- Day-04/target/debug/Day-04.d
+++
@@ -1,1 +0,0 @@
-C:\Users\phili\Documents\CodingProjekts\AoC-Rust\Day-04\target\debug\Day-04.exe: C:\Users\phili\Documents\CodingProjekts\AoC-Rust\Day-04\src\main.rs
--- Day-04/target/debug/deps/Day_04.d
+++
@@ -1,5 +0,0 @@
-C:\Users\phili\Documents\CodingProjekts\AoC-Rust\Day-04\target\debug\deps\Day_04.exe: src\main.rs
-
-C:\Users\phili\Documents\CodingProjekts\AoC-Rust\Day-04\target\debug\deps\Day_04.d: src\main.rs
-
-src\main.rs:
